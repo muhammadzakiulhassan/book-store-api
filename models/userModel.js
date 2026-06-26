d15be85ca11c400e7bb1c2b4225fb0cfb365e45e
Muhammad Zaki <muhammadzakiulhassan@users.noreply.github.com>
2026-06-27 00:55:03 +0500

    create user model with password hashing

---
+++ models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    validate: [validator.isEmail, 'please enter a valid email'],
+    lowercase: true,
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please enter your password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not same',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the password of the confirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword= async function(candidatePassword,userpassword){
+    return await bcrypt.compare(candidatePassword,userpassword)
+}
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
