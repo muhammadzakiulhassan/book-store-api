648f601d9ffae0642e715b204d009da9d854bd81
Muhammad Zaki <muhammadzakiulhassan@users.noreply.github.com>
2026-06-27 01:00:49 +0500

    Implement JWT signup and login authentication

---
+++ controllers/authController.js
@@ -0,0 +1,53 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXP_IN,
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = signToken(newUser.id);
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  //   const { email, password } = req.body;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // 1). Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('please provide email and password'));
+  }
+  // 2). Check if user exist and Password Correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect password or email', 401));
+  }
+
+  // 3.if everything is Ok send token to the client
+  const token = signToken(user.id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const userController = require('./../controllers/userController')
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 const router = express.Router();
 
+router.route('/signup').post(authController.signup);
+router.route('/login').post(authController.login)
+
 router
-.route(`/`)
+  .route(`/`)
   .get(userController.getAllUsers)
-  .post(userController.createUsers)
+  .post(userController.createUsers);
 router
   .route(`/:id`)
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
 
-  module.exports=router
+module.exports = router;
