9f4c0bb6c145bb80cd798545ff32d361a70d384d
Muhammad Zaki <muhammadzakiulhassan@users.noreply.github.com>
2026-07-04 01:12:57 +0500

    Implement update current user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,41 +1,75 @@
-const User=require('./../models/userModel')
-const catchAsync=require('./../utils/catchAsync')
-const AppError=require('./../utils/appError')
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
-exports. getAllUsers = catchAsync(async(req, res,next) => {
+const filteredObj = (obj, ...allowedFields) => {
+  newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
-  const users=await User.find();
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
   res.status(200).json({
     status: 'success',
-    result:users.length,
-   data:{
-    users
-   }
+    result: users.length,
+    data: {
+      users,
+    },
   });
 });
 
-exports. createUsers = (req, res) => {
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1). Create error if user post password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'The route is not for pasword update.Please use /updatePassword',
+      ),
+    );
+  }
+
+  // 2). Filtered allowed of unwanted fields names that are not to be updated
+  const filteredBody = filteredObj(req.body, 'name', 'email');
+  // 3). Update the user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredObj, {
+    new: true,
+    runValidators: true,
+  });
+
+  // 4). send the response
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+exports.createUsers = (req, res) => {
   res.status(505).json({
     status: 'err',
     message: 'This Route is not implemented yet',
   });
 };
 
-exports. getUser = (req, res) => {
+exports.getUser = (req, res) => {
   res.status(505).json({
     status: 'err',
     message: 'This Route is not implemented yet',
   });
 };
 
-exports. updateUser = (req, res) => {
+exports.updateUser = (req, res) => {
   res.status(505).json({
     status: 'err',
     message: 'This Route is not implemented yet',
   });
 };
 
-exports. deleteUser = (req, res) => {
+exports.deleteUser = (req, res) => {
   res.status(505).json({
     status: 'err',
     message: 'This Route is not implemented yet',
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,10 +4,15 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.route('/signup').post(authController.signup);
-router.route('/login').post(authController.login)
+router.route('/login').post(authController.login);
 
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
+router
+  .route('/updateMyPassword')
+  .patch(authController.protect, authController.updatePassword);
+
+  router.route('/updateMe').patch(authController.protect,userController.updateMe)
 
 router
   .route(`/`)
